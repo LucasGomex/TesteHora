716a53681eece66cb224ad86b1457c3c7c36844d
Lucas Gomes dos Santos <105444550+LucasGomex@users.noreply.github.com>
2023-06-12 15:40:26 -0300

    Update TesteHora.java

--- TesteHora.java
+++ TesteHora.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//Aluno: Lucas Gomes dos Santos
 public class TesteHora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
